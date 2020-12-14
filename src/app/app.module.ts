5ccab4b15e7392e0af3d5f44847bda2e2fab9ddd
Amartya Aishwarya <amartya@koverhoop.com>
2020-12-14 09:12:13 +0530

    basic outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ConfigFormComponent } from './config-form/config-form.component';
+import { EntryFormComponent } from './config-form/entry-form/entry-form.component';
+import { ExitFormComponent } from './config-form/exit-form/exit-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfigFormComponent,
+    EntryFormComponent,
+    ExitFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
