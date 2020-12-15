b433fdad25d07de05a83b37d5e2e5bbc1190beef
Amartya Aishwarya <amartya@koverhoop.com>
2020-12-15 18:49:07 +0530

    basic setup of form done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ConfigFormComponent } from './config-form/config-form.component';
@@ -16,7 +16,8 @@ import { ConfigFormComponent } from './config-form/config-form.component';
   imports: [
     BrowserModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/config-form/config-form.component.ts
+++ src/app/config-form/config-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, NgForm } from '@angular/forms';
 import { CamConfig } from 'src/shared/shared/cam-config.model';
+
 
 interface PolygonDetails{
   polygonCoordinates: string,
@@ -32,14 +33,68 @@ export class ConfigFormComponent implements OnInit {
 //     { "id": 2, "name": "Started" }
 // ];
 
+  polygonForm: FormGroup ;
+  polygonModel: FormArray ;
   cameraconfig: PolygonDetails[] = new Array<PolygonDetails>();
   @Input() config: CamConfig = new CamConfig('', null, null);
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    
+  }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
-  onSubmit(form: NgForm){
-    console.log(form);
+  // onSubmit(form: NgForm){
+  //   console.log(form);
+  // }
+
+  initForm(){
+    this.polygonForm = this.fb.group({
+      polygonModel: this.fb.array([
+        this.createNewPolygonModel()
+      ])
+    })
+
+    this.polygonModel = this.polygonForm.get('polygonModel') as FormArray;
   }
+
+
+  createNewPolygonModel(){
+    return this.fb.group({
+      polygon: [null],
+      entryAngle: this.fb.array([this.createNewAngle()]),
+      exitAngle: this.fb.array([this.createNewAngle()])
+    })
+  }
+
+
+
+  createNewAngle(){
+    return this.fb.group({
+      minRssi: [null],
+      maxRssi: [null]
+    })
+  }
+
+  addNewPolygonModel(){
+    // console.log(this.polygonForm);
+    this.polygonModel.push(this.createNewPolygonModel())
+  }
+
+  addEntryAngle(indexEntry: number){
+    (this.polygonModel.controls[indexEntry]?.get('entryAngle') as FormArray).push(this.createNewAngle())
+    // console.log(indexEntry)
+  }
+
+  addExitAngle(indexExit: number){
+    (this.polygonModel.controls[indexExit]?.get('exitAngle') as FormArray).push(this.createNewAngle())
+    // console.log(indexExit)
+  }
+
+  onSubmit(){
+    console.log(this.polygonForm.value);
+  }
+
+  // createNewExitAngle(){}
 }
