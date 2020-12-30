09154265a65c7867d9bf570a62f185f0362f532e
Amartya Aishwarya <amartya@koverhoop.com>
2020-12-30 14:17:56 +0530

    wip-download configs from form

--- src/app/config-form/config-form.component.ts
+++ src/app/config-form/config-form.component.ts
@@ -2,7 +2,12 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams,
+  HttpResponse
+} from '@angular/common/http';
 // import { CamConfig } from 'src/shared/shared/cam-config.model';
 
 // interface PolygonDetails{
@@ -94,99 +99,103 @@ export class ConfigFormComponent implements OnInit {
   }
 
   postFunction(obj: any): Observable<any> {
-    // const headers = new Headers();
-    // headers.append('Access-Control-Allow-Origin', '*');
-    // const headers = new HttpHeaders({
-    //   'Content-Type': 'application/json; charset=utf-8'
-    // });
-    // let headers = new HttpHeaders();
-    // headers = headers.set('Access-Control-Allow-Origin', '*');
-    // headers = headers.append(
-    //   'Access-Control-Allow-Methods',
-    //   'DELETE, POST, GET, OPTIONS'
-    // );
-    // headers = headers.append(
-    //   'Access-Control-Allow-Headers',
-    //   'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'
-    // );
-
-    // const httpOptions = {
-    //   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    // };
-    // const header = ['Access-Control-Allow-Origin', '*'];
     return this.httpclient.post<any>(
       `http://localhost:8000/download-config/abc/`,
       obj
     );
   }
 
+  // public downloadReport(res: HttpResponse<Blob>) {
+  //   const name = res.headers
+  //     .get('content-disposition')
+  //     ?.split(';')[1]
+  //     .split('filename')[1]
+  //     .split('=')[1]
+  //     .trim();
+  //   const url = window.URL.createObjectURL(res.body);
+  //   const a = document.createElement('a');
+  //   document.body.appendChild(a);
+  //   a.setAttribute('style', 'display: none');
+  //   a.href = url;
+  //   a.download = name!;
+  //   a.click();
+  //   window.URL.revokeObjectURL(url);
+  //   a.remove();
+  // }
+
   onSubmit() {
     let str = this.polygonForm.value;
-    let strStrigify = JSON.stringify(str);
+    // let strStrigify = JSON.stringify(str);
 
-    console.log(str);
-    console.log('strStrigify ', strStrigify);
-    console.log('str.polygonModel[0].polygon ', str.polygonModel[0].polygon);
-    console.log(
-      'str.polygonModel[0].entryAngle ',
-      str.polygonModel[0].entryAngle
-    );
-    console.log(
-      'str.polygonModel[0].exitAngle ',
-      str.polygonModel[0].exitAngle
-    );
-    console.log(
-      'str.polygonModel[0].radiusToInterpolate ',
-      str.polygonModel[0].radiusToInterpolate
-    );
-    console.log(
-      'str.polygonModel[0].minVectorMagnitude ',
-      str.polygonModel[0].minVectorMagnitude
-    );
-
-    const k = {
-      polygonModel: [
-        {
-          polygon:
-            'POLYGON((0.36515151515151517 0.22823529411764706, 0.5931818181818181 0.0058823529411764705, 0.8742424242424243 0.2211764705882353, 0.6909090909090909 0.6588235294117647, 0.3606060606060606 0.22705882352941176))',
-          entryAngle: [{ minRssi: 1160, maxRssi: 1236 }],
-          exitAngle: [
-            { minRssi: 1000, maxRssi: 1075 },
-            { minRssi: 1345, maxRssi: 1359 }
-          ],
-          radiusToInterpolate: null,
-          minVectorMagnitude: null
-        }
-      ]
-    };
+    // console.log(str);
 
     // const k = {
     //   polygonModel: [
     //     {
-    //       polygon: str.polygonModel[0].polygon,
-    //       entryAngle: str.polygonModel[0].entryAngle,
-    //       exitAngle: str.polygonModel[0].exitAngle,
-    //       radiusToInterpolate: str.polygonModel[0].radiusToInterpolate,
-    //       minVectorMagnitude: str.polygonModel[0].minVectorMagnitude
+    //       polygon:
+    //         'POLYGON((0.36515151515151517 0.22823529411764706, 0.5931818181818181 0.0058823529411764705, 0.8742424242424243 0.2211764705882353, 0.6909090909090909 0.6588235294117647, 0.3606060606060606 0.22705882352941176))',
+    //       entryAngle: [{ minRssi: 1160, maxRssi: 1236 }],
+    //       exitAngle: [
+    //         { minRssi: 1000, maxRssi: 1075 },
+    //         { minRssi: 1345, maxRssi: 1359 }
+    //       ],
+    //       radiusToInterpolate: null,
+    //       minVectorMagnitude: null
     //     }
     //   ]
     // };
 
-    this.postFunction(k).subscribe(response => {
+    // const k2 = {
+    //   polygonModel: [
+    //     {
+    //       polygon:
+    //         'POLYGON((0.36515151515151517 0.22823529411764706, 0.5931818181818181 0.0058823529411764705, 0.8742424242424243 0.2211764705882353, 0.6909090909090909 0.6588235294117647, 0.3606060606060606 0.22705882352941176))',
+    //       entryAngle: [{ minRssi: 1160, maxRssi: 1236 }],
+    //       exitAngle: [
+    //         { minRssi: 1000, maxRssi: 1075 },
+    //         { minRssi: 1257, maxRssi: 1359 }
+    //       ],
+    //       radiusToInterpolate: null,
+    //       minVectorMagnitude: null
+    //     }
+    //   ]
+    // };
+
+    const k_str = {
+      polygonModel: [
+        {
+          polygon: str.polygonModel[0].polygon,
+          entryAngle: str.polygonModel[0].entryAngle,
+          exitAngle: str.polygonModel[0].exitAngle,
+          radiusToInterpolate: str.polygonModel[0].radiusToInterpolate,
+          minVectorMagnitude: str.polygonModel[0].minVectorMagnitude
+        }
+      ]
+    };
+
+    console.log(JSON.stringify(k_str));
+
+    this.postFunction(k_str).subscribe(response => {
       // console.log(response);
       // console.log(k);
       this.resp = response;
-      console.log('this.resp', this.resp);
-      // var element = document.createElement('a');
-      // element.setAttribute(
-      //   'href',
-      //   'data:text/plain;charset=utf-8,' + encodeURIComponent(k)
-      // );
-      // element.setAttribute('download', 'config.json');
-      // element.style.display = 'none';
-      // document.body.appendChild(element);
-      // element.click();
-      // document.body.removeChild(element);
+      console.log('response ', response);
+      console.log(typeof response);
+      // this.downloadReport((response));
+
+      // this.downloadReport(response);
+      let test = JSON.stringify(response);
+      console.log(test);
+      var element = document.createElement('a');
+      element.setAttribute(
+        'href',
+        'data:text/plain;charset=utf-8,' + encodeURIComponent(test)
+      );
+      element.setAttribute('download', 'test_config.config');
+      element.style.display = 'none';
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
     });
   }
 }
